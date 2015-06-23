4ea66ddeb9a3125aac16178fafbcb16a5b95b62f
royschuyler <royschuyler@yahoo.com>
2015-06-23 09:28:08 -0500

    moving parts, ship animating

--- script.js
+++ script.js
@@ -1,31 +1,45 @@
 //load a sprite -- mave it default to sinking -- on keypress have it thrust up
 
-var game = new Phaser.Game(2000, 200, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+//make blocks come at player -- if collision, player explodes -- player starts over
+
+var game = new Phaser.Game(1000, 300, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
 
-    game.load.spritesheet('ship', 'assets/gfx/ship.png');
-    // game.load.image('sky', 'assets/gfx/ship.png');
-    // game.load.image('star', 'assets/star.png');
-    // game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+    game.load.spritesheet('ship', 'assets/gfx/ship.png', 32, 32);
+    game.load.spritesheet('block', 'assets/gfx/block.png');
+    game.load.spritesheet('explosion', '/assets/gfx/explosion.png', 128, 128);
 
 }
 
 function create() {
-  // game.add.sprite(0, 0, 'ship');
-  player = game.add.sprite(32, game.world.height - 150, 'ship')
+ //player
+  player = game.add.sprite(32,32, 'ship');
+  player.animations.add('up', [1]);
+  player.animations.add('down', [0]);
+
+
   game.physics.startSystem(Phaser.Physics.ARCADE);
   game.physics.arcade.enable(player);
   player.body.gravity.y = 300;
   player.body.collideWorldBounds = true;
 
+  player.body.bounce.setTo(0.25, 0.25);
 
 }
 
 function update() {
   cursors = game.input.keyboard.createCursorKeys();
 
-  if (cursors.up.isDown){
-    player.body.velocity.y = -100;
-  }
+      if (cursors.up.isDown){
+        player.body.velocity.y = -170;
+        player.animations.play('up');
+      }
+      else {
+        player.animations.play('down');
+      }
+
+
 }
+
+
