548a2d8be892443acdc04fa98da9c9aab9fe5a74
royschuyler <royschuyler@yahoo.com>
2015-06-23 11:23:55 -0500

    random blocks being fired on delay

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-//load a sprite -- mave it default to sinking -- on keypress have it thrust up XXXXXXXX
+//load a sprite -- make it default to sinking -- on keypress have it thrust up XXXXXXXX
 
 //make blocks come at player -- XXXXXXXX
 
-//make loop that makes block segments
+//make loop that makes block segments XXXXXXX
 
 //if collision, player explodes -- player starts over
 
+//make border with new image(smaller) no velocity no physics
+
+//make blockSegments exist at a random board height XXXXXXXXX
+
 var game = new Phaser.Game(1000, 300, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+var block;
 
 function preload() {
 
@@ -31,11 +37,31 @@ function create() {
   player.body.bounce.setTo(0.25, 0.25);
 
  //block
-  block = game.add.sprite(200,100, 'block');
+  // blockSegments = game.add.group();
+  function wait(){
+   setInterval(
+    function sendBlock() {
+        for (x = 0; x < game.rnd.integerInRange(1, 4); x += 1) {
+          var randomValue = game.rnd.integerInRange(32, 300);
+          block = game.add.sprite(game.width + 32, randomValue, 'block');
+          game.physics.arcade.enable(block);
+          block.body.collideWorldBounds = false;
+          block.body.velocity.x = -300;
+          // blockSegments.add(block);
+        };
+      },1000);
+  };
 
-  game.physics.arcade.enable(block);
-  block.body.collideWorldBounds = false;
-  block.body.velocity.x = -300;
+    wait();
+
+
+
+
+
+
+  // game.physics.arcade.enable(block);
+  // block.body.collideWorldBounds = false;
+  // block.body.velocity.x = -300;
 
 
 
@@ -50,12 +76,15 @@ function update() {
   cursors = game.input.keyboard.createCursorKeys();
 
       if (cursors.up.isDown){
-        player.body.velocity.y = -170;
+        player.body.velocity.y = -110;
         player.animations.play('up');
       }
       else {
         player.animations.play('down');
       }
+
+      // console.log(block.body.position.x)
+
 
 
 }
